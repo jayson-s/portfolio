ce9a3b6eae9181f4c2db62626973865ef40c15ec
Jayson Sandhu <jayson.sandhu@ontariotechu.net>
2025-02-27 15:26:04 -0500

    Updated website scaling on different devices

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
           defaultTheme="system"
         >
           <div className="flex min-h-screen w-full flex-col font-[family-name:var(--font-inter-tight)]">
-            <div className="relative mx-auto w-full max-w-screen-sm flex-1 px-4 pt-20">
+            <div className="relative mx-auto w-full max-w-screen-sm flex-1 px-4 pt-20 scale-100 md:scale-90 sm:scale-75 sm:w-[133%] origin-top">
               <Header />
               {children}
               <Footer />
